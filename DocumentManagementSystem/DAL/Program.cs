35625659fd329c5da11c62f057916be65d94af7f
XayosAT <102818529+XayosAT@users.noreply.github.com>
2024-10-14 19:40:07 +0200

    fixing DAL

--- DocumentManagementSystem/DAL/Program.cs
+++ DocumentManagementSystem/DAL/Program.cs
@@ -6,31 +6,31 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Migrations und Datenbankerstellung anwenden
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<DocumentContext>();
-
-    // Verbindungstest zur Datenbank
-    try
-    {
-        Console.WriteLine("Versuche, eine Verbindung zur Datenbank herzustellen...");
-
-        // Warte, bis die Datenbank bereit ist
-        while (!context.Database.CanConnect())
-        {
-            Console.WriteLine("Datenbank ist noch nicht bereit, warte...");
-            Thread.Sleep(1000); // Warte 1 Sekunde
-        }
-
-        Console.WriteLine("Verbindung zur Datenbank erfolgreich.");
-
-        // Migrations anwenden und die Datenbank erstellen/aktualisieren
-        context.Database.EnsureCreated();
-        // context.Database.Migrate();
-        Console.WriteLine("Datenbankmigrationen erfolgreich angewendet.");
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Fehler bei der Anwendung der Migrationen: {ex.Message}");
-    }
-}
+// using (var scope = app.Services.CreateScope())
+// {
+//     var context = scope.ServiceProvider.GetRequiredService<DocumentContext>();
+//
+//     // Verbindungstest zur Datenbank
+//     try
+//     {
+//         Console.WriteLine("Versuche, eine Verbindung zur Datenbank herzustellen...");
+//
+//         // Warte, bis die Datenbank bereit ist
+//         while (!context.Database.CanConnect())
+//         {
+//             Console.WriteLine("Datenbank ist noch nicht bereit, warte...");
+//             Thread.Sleep(1000); // Warte 1 Sekunde
+//         }
+//
+//         Console.WriteLine("Verbindung zur Datenbank erfolgreich.");
+//
+//         // Migrations anwenden und die Datenbank erstellen/aktualisieren
+//         context.Database.EnsureCreated();
+//         // context.Database.Migrate();
+//         Console.WriteLine("Datenbankmigrationen erfolgreich angewendet.");
+//     }
+//     catch (Exception ex)
+//     {
+//         Console.WriteLine($"Fehler bei der Anwendung der Migrationen: {ex.Message}");
+//     }
+// }
